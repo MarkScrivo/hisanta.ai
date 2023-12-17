bf38a34d123f1bf8ca7569793899d2f74466b05c
MarkScrivo <84589133+MarkScrivo@users.noreply.github.com>
2023-12-17 18:56:24 -0500

    Update index.tsx

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -9,9 +9,9 @@ export default function Header() {
     <header className="flex flex-row justify-between items-center top-0 mt-4">
       <div className="self-center text-3xl pl-4 mt-2 md:pl-8">
         <Link href="/">
-          <span className="text-Holiday-Green">Hi</span>
-          <span className="text-Holiday-Red">Santa</span>
-          <span>.ai</span>
+          <span className="text-Holiday-Green">OddsOn</span>
+          <span className="text-Holiday-Red">Compliance</span>
+          <span>.com</span>
         </Link>
       </div>
       <div className="ml-auto" />
